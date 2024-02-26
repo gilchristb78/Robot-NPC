5db15800f1f0c6a3653ec0be42f65b7b334e2450
Benjamin <gilchristb78@gmail.com>
2024-02-25 20:15:57 -0500

    Refactor / Performance / more settings
    Refactor away the wall check function and move parts of the code.
    also, ensure that unnecessary line traces aren't being run

--- Robot.cpp
+++ Robot.cpp
@@ -36,74 +36,94 @@ void ARobot::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
 	
+	MoveRobot(DeltaTime);
+
+}
+
+void ARobot::MoveRobot(float DeltaTime)
+{
+	if (!canProceed(DeltaTime)) //are we running into a wall?
+		return;
+
+
+	FVector CharacterLocation = Character->GetRelativeLocation() + GetActorLocation();	//where is our skeletalmesh?
+
+	DistanceAlongSpline = fmod(DistanceAlongSpline + (DeltaTime * Speed), Spline->GetSplineLength()); //advance
+	
+	//get next spot to move to
+	FVector Location = Spline->GetLocationAtDistanceAlongSpline(DistanceAlongSpline, ESplineCoordinateSpace::World); 
+	FRotator Rotation = Spline->GetRotationAtDistanceAlongSpline(DistanceAlongSpline, ESplineCoordinateSpace::World);
+
+	FHitResult OutHit;
+	FCollisionQueryParams Params;
+	Params.AddIgnoredActor(this);
+
+	FVector Start = Location;
+	Start.Z = CharacterLocation.Z + CharacterStepHeight;
+
+	FVector End = Location;
+	End.Z = CharacterLocation.Z - (CharacterStepHeight * 2);
+
+	GetWorld()->LineTraceSingleByChannel(OutHit, Start, End, ECC_Visibility, Params);	//find the ground
+
+	if (Debug)
+		DrawDebugLine(GetWorld(), Start, End, FColor::Red);
+
+	if (OutHit.bBlockingHit)
+	{
+		Location.Z = OutHit.Location.Z; //set the ground
+	}
+
+	if (CharacterLocation.Z - Location.Z < 0.1f && CharacterLocation.Z - Location.Z > -0.1f)
+	{
+		Speed = BaseSpeed;
+	}
+	else if (CharacterLocation.Z - Location.Z <= -0.1f)
+	{
+		Speed = BaseSpeed * (1.0 - SlopeSpeedModifier);	//Slope Up
+	}
+	else
+	{
+		Speed = BaseSpeed * (1 + SlopeSpeedModifier); //Slope Down
+	}
+
+	Rotation.Yaw -= 90; //look toward "forward" of spline
+
+	Character->SetWorldLocation(Location);
+	Character->SetWorldRotation(Rotation);
+
+}
+
+bool ARobot::canProceed(float DeltaTime)
+{
+	if(bHittingWall)
+		return false;
+
 	FVector CharacterLocation = Character->GetRelativeLocation() + GetActorLocation();
-
-	//UE_LOG(LogTemp, Warning, TEXT("Location: %f + %f + %f = %f"), Character->GetRelativeLocation().X, Spline->GetRelativeLocation().X, GetActorLocation().X, CharacterLocation.X)
-
-	float TempDistanceAlongSpline = fmod(DistanceAlongSpline + (DeltaTime * Speed), Spline->GetSplineLength());
-	float TempDistanceAlongSplineChecker = fmod(DistanceAlongSpline + (DeltaTime * Speed * 15), Spline->GetSplineLength());
-
-	FVector Location = Spline->GetLocationAtDistanceAlongSpline(TempDistanceAlongSpline, ESplineCoordinateSpace::World);
+	//could get weird with slow deltatime (running into a wall miles away)
+	//TODO change to go from next location (not multiplied) forward 1 then get that vector and multiply by the character depth
+	float TempDistanceAlongSplineChecker = fmod(DistanceAlongSpline + (DeltaTime * Speed * CharacterCheckDistance), Spline->GetSplineLength());
 	FVector LocationCheck = Spline->GetLocationAtDistanceAlongSpline(TempDistanceAlongSplineChecker, ESplineCoordinateSpace::World);
-	FRotator Rotation = Spline->GetRotationAtDistanceAlongSpline(TempDistanceAlongSpline, ESplineCoordinateSpace::World);
-
 	FVector Start = CharacterLocation;
-	Start.Z += 50;
+	Start.Z += CharacterStepHeight;
 
 	FVector End = LocationCheck;
-	End.Z = CharacterLocation.Z + 50;
-	
-	DrawDebugSphere(GetWorld(), CharacterLocation, 30, 15, FColor::Red);
-	DrawDebugLine(GetWorld(), Start, End, FColor::Red);
+	End.Z = CharacterLocation.Z + CharacterStepHeight;
+
+	if(Debug)
+		DrawDebugLine(GetWorld(), Start, End, FColor::Red);
 
 	FHitResult OutHit;
 	FCollisionQueryParams Params;
 	Params.AddIgnoredActor(this);
 
 	GetWorld()->LineTraceSingleByChannel(OutHit, Start, End, ECC_Visibility, Params);
-
 	if (OutHit.bBlockingHit)
 	{
-
-	}
-	else
-	{
-		DistanceAlongSpline = TempDistanceAlongSpline;
-		Start = Location;
-		Start.Z = CharacterLocation.Z + 50;
-
-		End = Location; 
-		End.Z = CharacterLocation.Z - 100;
-		GetWorld()->LineTraceSingleByChannel(OutHit, Start, End, ECC_Visibility, Params);
-
-		if (OutHit.bBlockingHit)
-		{
-			Location.Z = OutHit.Location.Z;
-		}
-
-		if (CharacterLocation.Z - Location.Z < 0.1f && CharacterLocation.Z - Location.Z > -0.1f)
-		{
-			Speed = BaseSpeed;
-		}
-		else if (CharacterLocation.Z - Location.Z <= -0.1f)
-		{
-			Speed = BaseSpeed * (1.0 - SlopeSpeedModifier);
-		}
-		else
-		{
-			Speed = BaseSpeed * (1 + SlopeSpeedModifier);
-		}
-
-		Rotation.Yaw -= 90;
-
-		Character->SetWorldLocation(Location);
-		Character->SetWorldRotation(Rotation);
+		bHittingWall = true;
+		return false;
 	}
 
-	
-
-
-
-	//UE_LOG(LogTemp, Warning, TEXT("Distance: %f"), DistanceAlongSpline);
+	return true;
 }
 
